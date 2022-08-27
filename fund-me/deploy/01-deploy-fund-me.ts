0d3ea2fbedf034fea918de4250c09bab1fc40bec
0xdeaner <deanjdominguez@gmail.com>
2022-08-27 11:11:43 -0600

    inital repo commit

---
+++ fund-me/deploy/01-deploy-fund-me.ts
@@ -0,0 +1,44 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
+import verify from "../utils/verify"
+import { networkConfig, developmentChains } from "../helper-hardhat-config"
+
+const deployFundMe: DeployFunction = async function(
+  hre: HardhatRuntimeEnvironment
+) {
+  const { getNamedAccounts, deployments, network } = hre
+  const { deploy, log } = deployments
+  const { deployer } = await getNamedAccounts()
+  const chainId: number = network.config.chainId!
+
+  let ethUsdPriceFeedAddress: string
+
+  if (chainId == 31337) {
+    const ethUsdAggregator = await deployments.get("MockV3Aggregator")
+    ethUsdPriceFeedAddress = ethUsdAggregator.address
+  } else {
+    ethUsdPriceFeedAddress = networkConfig[network.name].ethUsdPriceFeed!
+  }
+
+  log("----------------------------------------------------")
+  log("Deploying FundMe and waiting for confirmations...")
+  const fundMeArgs = [ethUsdPriceFeedAddress]
+  const fundMe = await deploy("FundMe", {
+    from: deployer,
+    args: fundMeArgs,
+    log: true,
+    // we need to wait if on a live network so we can verify properly
+    waitConfirmations: networkConfig[network.name].blockConfirmations || 0
+  })
+  log(`FundMe deployed at ${fundMe.address}`)
+
+  if (
+    chainId !== 31337 &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    await verify(fundMe.address, fundMeArgs)
+  }
+}
+
+export default deployFundMe
+deployFundMe.tags = ["all", "fundMe"]
---
+++ fund-me/hardhat.config.ts
@@ -0,0 +1,55 @@
+import("hardhat-gas-reporter")
+import("@nomiclabs/hardhat-etherscan")
+import("@nomicfoundation/hardhat-chai-matchers")
+import { task, HardhatUserConfig } from "hardhat/config"
+import "dotenv/config"
+import "solidity-coverage"
+import "@typechain/hardhat"
+import "@nomiclabs/hardhat-ethers"
+import "hardhat-deploy"
+
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  solidity: {
+    compilers:[
+      { version:"0.8.8" },
+      { version:"0.6.6" }
+    ]
+  },
+  networks: {
+    hardhat: {},
+    rinkeby: {
+      url: RINKEBY_RPC_URL!,
+      accounts: [PRIVATE_KEY!],
+      chainId: 4
+    },
+    localhost: {
+      url: "http://1270.0.0.1:8545",
+      chainId: 31337
+    }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY!
+  },
+  gasReporter: {
+    enabled: false,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    coinmarketcap: COINMARKETCAP_API_KEY!
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    }
+  }
+}
+
+export default config
+
+
